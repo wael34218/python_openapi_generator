839955e9446a57535f448848e6f1acd73b0e74a3
Wael Farhan <wael34218@gmail.com>
2019-03-02 15:42:17 +0200

    Initial commit

---
+++ openapi_generator.py
@@ -0,0 +1,105 @@
+import yaml
+import urllib
+import json
+
+
+class OpenapiGenerator():
+    types_map = {"int": "integer",
+                 "bool": "boolean",
+                 "float": "number",
+                 "str": "string",
+                 "list": "array",
+                 "dict": "object"}
+
+    def __init__(self, title, description, version, server=None):
+        self.configs = {
+            "openapi": "3.0.1",
+            "info": {"title": title, "description": description, "version": version},
+            "servers": ["https://swapi.co"]
+        }
+        self.paths = {}
+
+    def add_response(self, response):
+        self.paths[response.request.path_url] = {
+            response.request.method: {
+                'description': "Get this from docstring",
+                'parameters': self._get_parameters(response),
+                'requestBody': self._get_request_body(response),
+                'responses': self._get_response(response),
+                'servers': [response.request.url.replace(response.request.path_url, "")]
+            },
+            'servers': [response.request.url.replace(response.request.path_url, "")]
+        }
+        self.configs["paths"] = self.paths
+
+    def export(self, filename):
+        with open(filename, "w") as output_file:
+            output_file.write(yaml.dump(self.configs, default_flow_style=False))
+
+    def _get_request_body(self, response):
+        request_body = {}
+        if response.request.body:
+            if response.request.headers['Content-Type'] == 'application/json':
+                try:
+                    example = json.loads(response.request.body.decode('utf-8'))
+                    request_body['content'] = {
+                        'application/json': {
+                            'schema': {
+                                'type': 'object',
+                                'properties': {
+                                    k: {'type': self.types_map[v.__class__.__name__]}
+                                    for k, v in example.items()}
+                            },
+                            'example': {k: v for k, v in example.items()}
+                        }
+                    }
+                except ValueError:
+                    print("invalid json passed")
+        return request_body
+
+    def _get_response(self, response):
+        return {
+            response.status_code: {
+                'description': 'Get this from docstring',
+                'content': {
+                    response.headers['Content-Type']: {
+                        'schema': {
+                            'type': 'object',
+                            'properties': {
+                                k: {'type': self.types_map[v.__class__.__name__]}
+                                for k, v in response.json().items()}
+                        },
+                        'example': {k: v for k, v in response.json().items()}
+                    }
+                }
+            }
+        }
+
+    def _get_parameters(self, response):
+        # Adding parameters [query, headers, cookie].
+        # TODO: implement [path] parameters.
+        parameters = []
+        qs = urllib.parse.parse_qs(urllib.parse.urlparse(response.request.url).query)
+        for k, v in qs.items():
+            param = {'name': k,
+                     'in': 'query',
+                     'schema': {'type': self.types_map[v[0].__class__.__name__]},
+                     'example': v[0]}
+            parameters.append(param)
+
+        for k, v in response.request.headers.items():
+            if k in ['Accept', 'Connection', 'User-Agent', 'Accept-Encoding', 'Content-Length']:
+                continue
+            param = {'name': k,
+                     'in': 'header',
+                     'schema': {'type': self.types_map[v.__class__.__name__]},
+                     'example': v}
+            parameters.append(param)
+
+        for k, v in response.cookies.items():
+            param = {'name': k,
+                     'in': 'cookie',
+                     'schema': {'type': self.types_map[v.__class__.__name__]},
+                     'example': v}
+            parameters.append(param)
+        return parameters
---
+++ run.py
@@ -0,0 +1,14 @@
+from openapi_generator import OpenapiGenerator
+import requests
+
+# Step 1: Create an instance of the generator
+gen = OpenapiGenerator("Test", "This is a testing description", "0.0.1")
+
+# Step 2: Add all responses
+response = requests.get("https://swapi.co/api/planets/", params={"page": 2})
+gen.add_response(response)
+
+# Step 3:Export
+gen.export("example.yml")
+
+# Step 4: Profit
